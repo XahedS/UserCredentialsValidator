9bb80fe43b39718ff88979bafcb759562d6ef945
zahid <zahidsaleem0440@gmail.com>
2022-09-03 21:42:56 +0500

    16. Utilizing internationalizing concept, now reading messages.properties as well.

--- src/main/java/com/example/model/UserCredential.java
+++ src/main/java/com/example/model/UserCredential.java
@@ -1,14 +1,33 @@
 package com.example.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
 import javax.validation.constraints.Pattern;
+//import org.springframework.context.MessageSource;
+import com.example.config.ApiConfiguration;
 
 @Entity
 @Table(name="usercredential")
 //@Validated
 public class UserCredential {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private boolean registrationStatus = false;
+    @Column(unique = true)
+    private String email;
+    @Pattern(regexp = "^[a-zA-Z0-9]*$",
+                    message = "{error.username.message}")// "username must be of 6 to 12 length with no special characters") //Put the messages in external file; message.properties
+    @Column(name = "username", unique = true)
+    private String name;
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$",
+            message = "password must contain atleast 1 uppercase, 1 lowercase, 1 digit & should be of min length 8 ")
+    private String password;
+
     public UserCredential(){
     }
 
@@ -19,20 +38,6 @@ public class UserCredential {
         this.password = password;
         this.registrationStatus = false;
     }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    private boolean registrationStatus = false;
-    @Column(unique = true)
-    private String email;
-    @Pattern(regexp = "^[a-zA-Z0-9]*$",
-                    message = "username must be of 6 to 12 length with no special characters") //Put the messages in external file; message.properties
-    @Column(name = "username", unique = true)
-    private String name;
-    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$",
-            message = "password must contain atleast 1 uppercase, 1 lowercase, 1 digit & should be of min length 8 ")
-    private String password;
 
     public int getId() {
         return id;
